d6cb263a3c8dce76f562a9d5986ccabfed3e389b
shervin920512 <sh.aliari92@gmail.com>
2025-05-16 06:52:01 +0330

    Delete pages/index.js

--- pages/index.js
+++
@@ -1,8 +0,0 @@
-export default function Home() {
-  return (
-    <main style={{ fontFamily: 'sans-serif', padding: 20 }}>
-      <h1>BreakBuilder</h1>
-      <p>اپ تمرینی اسنوکر شروین</p>
-    </main>
-  );
-}
