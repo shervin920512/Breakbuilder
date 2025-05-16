21aa2f41980b8ce6ad4d02b2880f17342748067b
shervin920512 <sh.aliari92@gmail.com>
2025-05-16 06:52:58 +0330

    Create index.js

---
+++ pages/index.js
@@ -0,0 +1,8 @@
+export default function Home() {
+  return (
+    <main style={{ fontFamily: 'sans-serif', padding: 20 }}>
+      <h1>BreakBuilder</h1>
+      <p>اپ تمرینی اسنوکر شروین</p>
+    </main>
+  );
+}
