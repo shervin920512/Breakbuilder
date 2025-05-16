2f5eb49750d45c3f870ae64119171182d709305a
shervin920512 <sh.aliari92@gmail.com>
2025-05-16 06:40:46 +0330

    Delete index.js

--- index.js
+++
@@ -1,8 +0,0 @@
-export default function Home() {
-  return (
-    <div style={{ fontFamily: 'sans-serif', padding: 20 }}>
-      <h1>BreakBuilder</h1>
-      <p>اپ تمرین روزانه اسنوکر</p>
-    </div>
-  );
-}
